de784928ee736dbc46836d7719cbc46dbf632306
819345855 <8879jshady@163.com>
2021-02-02 18:25:00 +0800

    first commit

--- NO_PROXY_Capture.js
+++ NO_PROXY_Capture.js
@@ -6,7 +6,7 @@ Java.perform(function() {
     var type = Java.use("java.net.Proxy$Type");
     var isa = Java.use("java.net.InetSocketAddress");
     okHttp.proxy.overload("java.net.Proxy").implementation = function() {
-        var sa = isa.$new("172.31.253.221", 7777); //此处为 自己代理的IP和端口号
+        var sa = isa.$new("ip", 端口); //此处为 自己代理的IP和端口号
         console.log("设置代理："+sa);
         var myproxy = jproxy.$new(type.HTTP.value, sa);
         arguments[0] = myproxy;
